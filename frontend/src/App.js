773a01389e81b8ebe94930cd1d7487de09f0bdca
Petra Michael <petraxmichael@gmail.com>
2024-11-16 11:33:13 +0700

    v0.2.9 copied from main branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-// import { LoginSignUp } from './Components/LoginSignUp/LoginSignUp';
 import Homepage from './Components/Homepage.jsx'
 // import ProceduralTree from './ProceduralTree';
+import Dashboard from './Components/Dashboard.jsx'
 function App() {
   return (
     <div >
       {/* <LoginSignUp/> */}
+      {/* <Homepage /> */}
+      <Dashboard />
       <Homepage />
     </div>
   );
