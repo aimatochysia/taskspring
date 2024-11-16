a6506dcf74ee9cfe60200fb9cd4fcbe072bd516d
Petra Michael <petraxmichael@gmail.com>
2024-11-16 11:31:36 +0700

    v0.2.9 added necessary packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
-// import { LoginSignUp } from './Components/LoginSignUp/LoginSignUp';
-<<<<<<< HEAD
-// import Homepage from './Components/Homepage.jsx'
-=======
 import Homepage from './Components/Homepage.jsx'
->>>>>>> ed31df0130bed8b837e899e42328d14e2b2e475b
 // import ProceduralTree from './ProceduralTree';
 import Dashboard from './Components/Dashboard.jsx'
 function App() {
   return (
     <div >
       {/* <LoginSignUp/> */}
-<<<<<<< HEAD
       {/* <Homepage /> */}
       <Dashboard />
-=======
       <Homepage />
->>>>>>> ed31df0130bed8b837e899e42328d14e2b2e475b
     </div>
   );
 }
