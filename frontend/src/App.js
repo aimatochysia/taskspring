93ff69cdaea53a065afced29742553a07b29650e
Aulia Yasmin <auliaaayy@gmail.com>
2024-11-21 21:54:05 +0700

    v0.6.0
    page help, setting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
-import Homepage from './Components/Homepage.jsx'
+import React from 'react';
+import './Components/LoginSignUp/LoginSignUp.css';
+import { LoginSignUp } from './Components/LoginSignUp/LoginSignUp.jsx';
+// import Homepage from './Components/Homepage.jsx';
 // import ProceduralTree from './ProceduralTree';
-import Dashboard from './Components/Dashboard.jsx'
+// import Task from './Components/Task.jsx';
+// import Dashboard from './Components/Dashboard.jsx';
+// import Board from './Components/Board.jsx';
+// import City from './Components/City';
+// import SettingsPage from './Components/Settings.jsx';
+// import { LanguageProvider } from './context/LanguageContext.js';
+// import HelpPage from './Components/Help.jsx';
+
 function App() {
   return (
     <div >
-      {/* <LoginSignUp/> */}
+      <LoginSignUp/>
       {/* <Homepage /> */}
-      <Dashboard />
-      <Homepage />
+      {/* <Task /> */}
+      {/* <Homepage /> */}
+      {/* <Dashboard /> */}
+      {/* <Board /> */}
+      {/* <City /> */}
+      {/* <LanguageProvider>
+      <SettingsPage />
+      </LanguageProvider> */}
+      {/* <HelpPage /> */}
+
     </div>
   );
 }
+
 
 export default App;
